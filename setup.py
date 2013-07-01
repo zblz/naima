70c1b62ccd392b593952420b9e8b3340abe93473
Víctor Zabalza <vzabalza@gmail.com>
2013-07-01 17:11:51 +0200

    ad setup, readme

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(name='emcee_specfit',
+      version='0.1',
+      description='MCMC spectral fitting with emcee',
+      author='Víctor Zabalza',
+      author_email='vzabalza@gmail.com',
+      py_modules=["emcee_specfit"],
+      long_descritpion=open("README.md").read(),
+      requires = [
+          'emcee>=1.2.0',
+          'triangle_plot',
+          'numpy',
+          'matplotlib',
+          'astropy>=0.2',
+          'scipy',
+          ]
+     )
