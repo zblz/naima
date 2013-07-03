2a0b7a4c720a1bc5e350918e7f3a0b949eab83ac
Víctor Zabalza <vzabalza@gmail.com>
2013-07-03 14:18:16 +0200

    simplify OZM

--- onezone.py
+++ onezone.py
@@ -45,7 +45,6 @@ heaviside=lambda x: (np.sign(x)+1)/2.
 class OneZoneModel:
 
     def __init__(self,nolog=0,debug=0,**kwargs):
-        # redirigir el logger a les funcions bogus de sota si ens crida multiprocessing
         if nolog:
             self.logger=self
         else:
@@ -65,7 +64,8 @@ class OneZoneModel:
 
     def _set_default(self,attrlist):
         """
-        Sets the attribute to its default value given by DEF_attr
+        Sets the attributes in attrlist to their default value given by DEF_attr
+        if they has not already been set as self.attr
         """
         if type(attrlist)=='str':
             attrlist=[attrlist,]
@@ -89,16 +89,12 @@ class OneZoneModel:
     DEF_gmax  = 3e9
     DEF_ngamd = 400 # electron spectrum points per decade
     # Injection spectrum
-    DEF_Linj      = 1e36 #erg/s
-    DEF_gammainj  = 2
-    DEF_glocut    = 1e7/mec2eV # minimum gamma*mec2 = 10MeV
-    DEF_cutoffidx = 1.
-    # fixed ghicut
-    DEF_fixed_ghicut = False
-    DEF_ghicut       = 1e8
-    # fixed injection normalisation (useful for fitting)
-    DEF_inj_norm     = None
+    DEF_inj_norm     = 1e35
     DEF_inj_norm_gam = 2e5 # ~100 GeV
+    DEF_gammainj     = 2
+    DEF_glocut       = 1e7/mec2eV  # sharp low energy cutoff at gamma*mec2 = 10MeV
+    DEF_ghicut       = 2e13/mec2eV # exponential high energy cutoff at gamma*mec2 = 20TeV
+    DEF_cutoffidx    = 1.          # Exponent of exponential cutoff
     # evolve spectrum to steady-state?
     DEF_evolve_nelec=True
     # Emitted spectrum matrix (eV)
@@ -220,67 +216,18 @@ class OneZoneModel:
                np.log10(self.emax/self.emin)*self.nened)
         self.outspecerg=self.outspecene/eV
 
-    def calc_ghicut(self):
-        """
-        Compute electron spectrum high energy cutoff from analytical formulae for
-        IC-KN and syn losses
-        """
-        # only apply to star seedspecs, as others are not in KN
-        try:
-            idx=self.seedspec.index('star')
-        except ValueError:
-            self.ghicut=self.gmax
-            return None
-        gmaxsyn = 2.*self.B**(-1./2.)*(self.eta/1.e3)**(-1./2.)/mec2TeV # emc2
-        if self.bb:
-            pheint=trapz(self.phn[idx],self.photE[idx])
-        else:
-            pheint=self.phe[idx]
-        gmaxic  = 9.e5*(self.B/pheint)**3.3*(self.eta/1.e3)**(-3.3)/mec2TeV # emc2
-        self.ghicut=float(np.minimum(gmaxic,gmaxsyn))
-
-    def calc_qinj(self,norm10G=False):
+    def _calc_qinj(self):
         """
         Compute injection spectrum, return as array.
         """
-        self._set_default(['gammainj','glocut','cutoffidx','Linj','inj_norm','inj_norm_gam','remit','fixed_ghicut'])
+        self._set_default(['gammainj','glocut','ghicut','cutoffidx','inj_norm','inj_norm_gam','fixed_ghicut'])
 
-        if self.fixed_ghicut:
-            self._set_default(['ghicut',])
-        else:
-            try:
-                ghicut_arg=int(np.where(np.diff(np.sign(self.ttot-self.tacc)))[0])
-            except TypeError:
-                ghicut_arg=len(self.gam)
-
-            if ghicut_arg<len(self.gam)-1:
-                self.ghicut=self.gam[ghicut_arg]
-                self.logger.debug('Cutoff obtained from loss timescale comparison: gamma = {0:.2e}; {1:.2e} eV'.format(self.ghicut,self.ghicut*mec2eV))
-            else:
-                self.calc_ghicut()
-                self.logger.debug('Cutoff obtained from analytical formulae: gamma = {0:.2e}; {1:.2e} eV'.format(self.ghicut,self.ghicut*mec2eV))
-    
-        if self.inj_norm!=None:
-            qinj=self.inj_norm*(self.gam/self.inj_norm_gam)**-self.gammainj*np.exp(-(self.gam/self.ghicut)**self.cutoffidx)
-            qinj[np.where(self.gam<self.glocut)]=0.
-        else:
-            qinj=(self.gam)**-self.gammainj*np.exp(-(self.gam/self.ghicut)**self.cutoffidx)
-            qinj[np.where(self.gam<self.glocut)]=0.
-            qinj*=self.Linj/trapz(qinj*self.gam*mec2,self.gam)
+        qinj=self.inj_norm*(self.gam/self.inj_norm_gam)**-self.gammainj*np.exp(-(self.gam/self.ghicut)**self.cutoffidx)
+        qinj[np.where(self.gam<self.glocut)]=0.
 
         self.logger.debug('calc_qinj:Injected power: %g erg/s'%(trapz(qinj*self.gam*mec2,self.gam)))
-#convertir-ho a densitat
-        qinj/=self.vemit()
         return qinj
 
-    def equipartition_B(self):
-        """
-        Compute equipartition B from U_B=U_rel
-        """
-        Urel=np.trapz(self.nelec*self.gam*mec2,self.gam)
-        return np.sqrt(Urel*8.*np.pi)
-
-#    @profile
     def calc_gdot(self):
 # Calculem Qinj i generem el self.gam correcte!
         if not hasattr(self,'gam'):
@@ -337,9 +284,9 @@ class OneZoneModel:
     def evolve_e_spectrum(self):
         self._set_default(['evolve_nelec',])
         self.calc_gdot()
-        qinj = self.calc_qinj()
+        qinj = self._calc_qinj()
         if self.evolve_nelec:
-#calculem els trapz terms i despres nomes els sumem
+            # calculem els trapz terms i despres nomes els sumem
             dgam=np.diff(self.gam)
             tt=dgam*(qinj[1:]+qinj[:-1])/2.
             qint = np.array([np.sum(tt[i:]) for i in range(len(qinj))])
@@ -349,7 +296,7 @@ class OneZoneModel:
             self.nelec = qinj
 
         self.logger.debug('Total energy in electrons: {0:.2e} erg'.format(
-            self.vemit()*trapz(self.nelec*self.gam*mec2,self.gam)))
+            trapz(self.nelec*self.gam*mec2,self.gam)))
 
     def calc_sy(self):
         self.logger.debug('calc_sy')
@@ -367,6 +314,7 @@ class OneZoneModel:
             self.calc_nelec()
 
         enehz = self.outspecene/hz2ev
+        nelec = self.nelec/self.vemit() # density required for sync calculation
 
         freqcrit=CS1*(self.gam*mec2)**2.*self.B
         xx=np.vstack(enehz)/freqcrit # shape of (outspecene,gam)
@@ -375,11 +323,11 @@ class OneZoneModel:
         #Fx=np.where(xx<10.,1.85*cbrt(xx)*np.exp(-xx),0) # 16.1 ms per loop
 
         Psy=CS3*self.B*Fx #Pot sync radiada per un elec, shape of (outspecene,gam)
-        J=4.*np.pi*trapz(Psy*self.nelec,self.gam) # int over gam, shape of outspecene
+        J=4.*np.pi*trapz(Psy*nelec,self.gam) # int over gam, shape of outspecene
 
         # Absorption
         CA1=(c**2./enehz**2.)*CS3*self.B # shape of outspecene
-        CA2=self.nelec/(self.gam*mec2) # shape of gam
+        CA2=nelec/(self.gam*mec2) # shape of gam
         CA3=Fx*(1./3.+2.*xx) # shape of (outspecene,gam)
         sabs=np.vstack(CA1)*CA2*CA3 # shape of (outspecene,gam)
         K=trapz(sabs,self.gam) #int over gamma, shape of outspecene
@@ -416,11 +364,9 @@ class OneZoneModel:
         photE0=(photE/mec2)[:,np.newaxis]
         #phn=(phn)[:,np.newaxis]
 
-        #self.logger.debug('{0} {1}'.format(photE.shape,photE0.shape))
         b=4*photE0*self.gam
-# Iterem sobre Eph per estalviar memoria
-# TODO: ho hauriem de fer sobre nbb per minimitzar el numero de loops (o fer-ho piece-wise, però és un merder)
         iclum=np.zeros_like(self.outspecene)
+# Iterate over Eph per save memory
         for i,Eph in enumerate(self.outspecene/mec2eV):
             w=Eph/self.gam
             q=w/(b*(1-w))
@@ -443,7 +389,7 @@ class OneZoneModel:
             iclum[i]=(3./4.)*sigt*c*(Eph*mec2)*lum
 
         #iclum[np.isnan(iclum)]=0.
-        return iclum*self.vemit()
+        return iclum
 
     def calc_ic(self):
         self.logger.debug('calc_ic')
@@ -466,6 +412,7 @@ class OneZoneModel:
 
         if self.ssc:
             self.calc_sy()
+# compute density of synchrotron photons in rsync radius
             rsyn=self.remit
             umean=2.24
             syf=umean/(4*np.pi*rsyn**2*c)
@@ -486,7 +433,6 @@ class OneZoneModel:
                 for ll in [self.seedspec,self.phe,self.photE,self.phn]:
                     del ll[idx]
 
-
         for idx,seedspec in enumerate(self.seedspec):
             # Call actual computation, detached to allow changes in subclasses
             specic=self._calc_specic(phn=self.phn[idx],photE=self.photE[idx],seed=seedspec)
@@ -498,142 +444,20 @@ class OneZoneModel:
 
         self.logger.debug('self.specic.shape={0}'.format(self.specic.shape))
         toticlum=trapz(self.specic,self.outspecerg)
+        self.logger.info('IC emitted power: %e erg/s'%toticlum)
         tev=np.where(self.outspecene>1e11)
         tottevlum=trapz(self.specic[tev],self.outspecerg[tev])
-        self.logger.info('IC emitted power: %e erg/s'%toticlum)
         self.logger.info('TeV (>100 GeV) luminosity: %e erg/s'%tottevlum)
-
-    def fitPL(self,ener,spec,ran):
-        from scipy.optimize import curve_fit
-        def linpl(x,N,alpha):
-            return np.log10(N)-alpha*x
-        pars,covar=curve_fit(linpl,np.log10(ener[ran]),np.log10(spec[ran]),
-                p0=[1e33,2.5])
-        return pars[0],pars[1]
-
-    def calc_lums(self):
-        if not hasattr(self,'specsy'):
-            self.logger.warn('No specsy, calling calc_sy()...')
-            self.calc_sy()
-        if not hasattr(self,'specic'):
-            self.logger.warn('No specic, calling calc_ic()...')
-            self.calc_ic()
-
-        if hasattr(self,'specicabs'):
-            ppabs=True
-        else:
-            if hasattr(self,'calc_ppabs'):
-                self.logger.warn('No specicabs, calling calc_ppabs()...')
-                self.calc_ppabs()
-                ppabs=True
-            else:
-                ppabs=False
-
-        self.toticlum=trapz(self.specic,self.outspecene/eV)
-        if ppabs:
-            self.toticabslum=trapz(self.specicabs,self.outspecene/eV)
-        self.totsylum=trapz(self.specsy,self.outspecene/eV)
-        xray=np.where((self.outspecene>300)&(self.outspecene<10000))
-        gev=np.where((self.outspecene>0.1e9)&(self.outspecene<10e9))
-        fermi=np.where(self.outspecene>0.1e9)
-        tev=np.where(self.outspecene>0.1e12)
-        hess=np.where((self.outspecene>0.2e12)&(self.outspecene<5e12))
-        totspec=(self.specic+self.specsy)
-        if np.sum(totspec[xray]>0.):
-            self.xlum=trapz(totspec[xray],self.outspecerg[xray]) # erg/s
-            self.xnorm,self.xidx=self.fitPL(self.outspecene/1e3,totspec,xray)
-        else:
-            self.xlum=0.
-        if np.sum(totspec[gev]>0.):
-            self.gevlum=trapz(totspec[gev],self.outspecerg[gev]) # erg/s for 100MeV<E<10GeV
-        else:
-            self.gevlum=0.
-        if np.sum(totspec[fermi]>0.):
-            self.fermilum=trapz(totspec[fermi]/self.outspecerg[fermi],self.outspecerg[fermi]) # ph/s for E>100MeV
-        else:
-            self.fermilum=0.
-        if np.sum(totspec[tev]>0.):
-            self.tevlum=trapz(totspec[tev],self.outspecerg[tev]) # erg/s
-            if ppabs:
-                self.tevabslum=trapz(self.specicabs[tev],self.outspecerg[tev]) # erg/s
-            tevfit=np.where((self.outspecene>0.1e12)&(self.outspecene<4e12)&(totspec>0))
-        else:
-            self.tevlum=0.
-            if ppabs: 
-                self.tevabslum=0.
-        if np.sum(totspec[hess]>0.):
-            self.hessphlum=trapz(totspec[hess]/self.outspecerg[hess],self.outspecerg[hess]) # ph/s for E>1TeV
-            self.hesslum=trapz(totspec[hess],self.outspecerg[hess]) # erg/s for E>1TeV
-        else:
-            self.hessphlum=0.
-            self.hesslum=0.
-
-        self.logger.info('*** Sync ***')
-        self.logger.info('Total synchrotron luminosity:  {0:.2e} erg/s'.format(self.totsylum))
-        self.logger.info('X-ray (0.3-10 keV) luminosity: {0:.2e} erg/s'.format(self.xlum))
-        self.logger.info('*** IC ***')
-        self.logger.info('Total IC luminosity:           {0:.2e} erg/s'.format(self.toticlum))
-        self.logger.info('VHE (>100 GeV) luminosity:     {0:.2e} erg/s'.format(self.tevlum))
-        if ppabs:
-            self.logger.info('VHE (>100 GeV) abs luminosity: {0:.2e} erg/s'.format(self.tevabslum))
-        self.logger.info(' HE (>0.1 GeV) luminosity:     {0:.2e} erg/s'.format(self.gevlum))
-
-    def calc_fluxes(self):
-        if not hasattr(self,'tevlum'):
-            self.logger.warn('No tevlum, calling calc_lums()...')
-            self.calc_lums()
-
-        if hasattr(self,'tevabslum'):
-            ppabs=True
-        else:
-            ppabs=False
-
-        if not hasattr(self,'dist'):
-            self.logger.warn('System has no distance, not computing fluxes')
-            return
-
-        dfac=4.*np.pi*self.dist**2.
-        self.xflux=self.xlum/dfac
-        self.gevflux=self.gevlum/dfac
-        self.fermiflux=self.fermilum/dfac
-        self.tevflux=self.tevlum/dfac
-        if ppabs:
-            self.tevabsflux=self.tevabslum/dfac
-        self.hessphflux=self.hessphlum/dfac
-        self.hessflux=self.hesslum/dfac
-
-        self.logger.info('*** Fluxes ***')
-        self.logger.info('X-ray (0.3-10 keV) flux:       {0:.2e} erg/s/cm2'.format(self.xflux))
-        self.logger.info('VHE (>100 GeV) flux:           {0:.2e} erg/s/cm2'.format(self.tevflux))
-        if ppabs:
-            self.logger.info('VHE (>100 GeV) abs flux:       {0:.2e} erg/s/cm2'.format(self.tevabsflux))
-        self.logger.info(' HE (>0.1 GeV) flux:           {0:.2e} erg/s/cm2'.format(self.gevflux))
-
-    equipartition=0
 
     def calc_nelec(self):
         if not hasattr(self,'phe'):
             self.calc_seedspec()
         self.generate_gam()
-        if self.equipartition:
-            self.calc_qinj()
-            self.evolve_e_spectrum()
-            relchange=1.
-            while relchange>1e-3:
-#                self.B=self.equipartition_B()
-                self.evolve_e_spectrum()
-                Bequi=self.equipartition_B()
-                relchange=abs(Bequi-self.B)/self.B
-                self.logger.debug('Bold: {0:.2f}, Beq: {1:.2f}, Change: {2:.5f}'.format(self.B,Bequi,relchange*100))
-                self.B=(0.25*self.B+0.75*Bequi)
-#                self.B=Bequi
-            self.logger.info('Equipartition magnetic field: {0:.3f} G'.format(self.B))
-        else:
-            self.evolve_e_spectrum()
+        self.evolve_e_spectrum()
 
     def calc_outspec(self):
         self.generate_outspecene()
+        self.calc_nelec()
         self.calc_sy()
         self.calc_ic()
-        self.calc_lums()
 
