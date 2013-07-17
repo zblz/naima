f9ad9d9c4c8093555769a13abbb857dc228f2ad2
Victor Zabalza <vzabalza@gmail.com>
2013-07-17 15:38:23 +0200

    add a few tests

---
+++ gammafit/tests/test_onezone.py
@@ -0,0 +1,34 @@
+from astropy import units as u
+import numpy as np
+
+def test_electronozm():
+    from ..onezone import ElectronOZM
+
+    ozm = ElectronOZM( np.logspace(0,15,1000), 1, seedspec=['CMB',], bb=True,nbb=10,index=2.0,cutoff=1e13,beta=1.0)
+    ozm.calc_outspec()
+
+    lsy=np.trapz(ozm.specsy*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
+    np.testing.assert_almost_equal(lsy,0.071974056347549428)
+    lic=np.trapz(ozm.specic*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
+    np.testing.assert_almost_equal(lic,212255584.80106944)
+
+def test_electronozm_evolve():
+    from ..onezone import ElectronOZM
+
+    ozm = ElectronOZM( np.logspace(0,15,1000), 1, seedspec=['CMB',], bb=True,nbb=10,index=2.0,cutoff=1e13,beta=1.0,evolve_nelec=True)
+    ozm.calc_outspec()
+
+    lsy=np.trapz(ozm.specsy*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
+    np.testing.assert_almost_equal(lsy,24600421942.782776)
+    lic=np.trapz(ozm.specic*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
+    np.testing.assert_almost_equal(lic,1.0455302369906613e+20)
+
+def test_protonozm():
+    from ..onezone import ProtonOZM
+
+    ozm = ProtonOZM( np.logspace(8,15,100), 1, index=2.0,cutoff=1e13,beta=1.0)
+    ozm.calc_outspec()
+
+    lpp=np.trapz(ozm.specpp*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
+    np.testing.assert_almost_equal(lpp,3.2800627079738687e+23)
+
