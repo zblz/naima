9977abf66a621740d49e519e0c6da47a47b28244
Victor Zabalza <vzabalza@gmail.com>
2013-07-18 11:26:59 +0200

    change test assertion to relative difference instead of absolute

--- gammafit/tests/test_onezone.py
+++ gammafit/tests/test_onezone.py
@@ -1,6 +1,9 @@
 from astropy import units as u
 import numpy as np
 
+def assert_almost_equal(x,y):
+    assert abs(x-y)/y < 1e-6
+
 def test_electronozm():
     from ..onezone import ElectronOZM
 
@@ -8,9 +11,9 @@ def test_electronozm():
     ozm.calc_outspec()
 
     lsy=np.trapz(ozm.specsy*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
-    np.testing.assert_almost_equal(lsy,0.071974056347549428)
+    assert_almost_equal(lsy,0.071974056347549428)
     lic=np.trapz(ozm.specic*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
-    np.testing.assert_almost_equal(lic,212255584.80106944)
+    assert_almost_equal(lic,212255584.80106944)
 
 def test_electronozm_evolve():
     from ..onezone import ElectronOZM
@@ -19,9 +22,9 @@ def test_electronozm_evolve():
     ozm.calc_outspec()
 
     lsy=np.trapz(ozm.specsy*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
-    np.testing.assert_almost_equal(lsy,24600421942.782776)
+    assert_almost_equal(lsy,24600421942.782776)
     lic=np.trapz(ozm.specic*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
-    np.testing.assert_almost_equal(lic,1.0455302369906613e+20)
+    assert_almost_equal(lic,1.0455302369906613e+20)
 
 def test_protonozm():
     from ..onezone import ProtonOZM
@@ -30,5 +33,5 @@ def test_protonozm():
     ozm.calc_outspec()
 
     lpp=np.trapz(ozm.specpp*ozm.outspecene**2*u.eV.to('erg'),ozm.outspecene)
-    np.testing.assert_almost_equal(lpp,3.2800627079738687e+23)
+    assert_almost_equal(lpp,3.2800627079738687e+23)
 
