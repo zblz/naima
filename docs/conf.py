df1e691e9bb2a050d4d3fd023a3a23bb13caa9a5
Víctor Zabalza <vzabalza@gmail.com>
2013-11-22 15:22:23 +0000

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,12 @@ release = gammafit.__version__
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes. To override the custom theme, set this to the
 # name of a builtin theme or the name of a custom theme in html_theme_path.
-#html_theme = None
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+#else:
+    #html_theme = 'nature'
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
