a75b7d53f8ec05bd8a4e9ebf0e50439a4c948554
Christoph Deil <Deil.Christoph@gmail.com>
2014-04-11 17:58:14 +0200

    Add package docstring.
    (I might have removed that one accidentally before)

--- gammafit/__init__.py
+++ gammafit/__init__.py
@@ -1,7 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is an Astropy affiliated package.
+`gammafit` uses MCMC fitting of non-thermal X-ray, GeV, and TeV spectra
+to constrain the properties of their parent relativistic particle distributions.
+
+The workhorse of gammafit is the powerful `~emcee`
+affine-invariant ensemble sampler for Markov chain Monte Carlo.
 """
 
 # Affiliated packages may add whatever they like to this file, but
