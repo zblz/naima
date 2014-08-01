34cd6622aa2ffa6b01a960afcf3aeaa8d1f3420b
Víctor Zabalza <vzabalza@gmail.com>
2014-08-01 16:55:53 +0100

    fixed sherpamod Sycnhrotron and PionDecay

--- gammafit/sherpamod.py
+++ gammafit/sherpamod.py
@@ -35,7 +35,7 @@ class InverseCompton(ArithmeticModel):
     def __init__(self,name='IC'):
         self.index   = Parameter(name, 'index', 2.0, min=-10, max=10)
         self.ref     = Parameter(name, 'ref', 20, min=0, frozen=True, units='TeV')
-        self.ampl    = Parameter(name, 'ampl', 1, min=0, units='1/eV')
+        self.ampl    = Parameter(name, 'ampl', 1, min=0, max=1e100, units='1/eV')
         self.cutoff  = Parameter(name, 'cutoff', 0.0, min=0,frozen=True, units='TeV')
         self.beta    = Parameter(name, 'beta', 1, min=0, max=10, frozen=True)
         self.TFIR    = Parameter(name, 'TFIR', 70, min=0, frozen=True, units='K')
@@ -83,132 +83,122 @@ class InverseCompton(ArithmeticModel):
         ic = models.InverseCompton(pdist, seed_photon_fields=seedspec,
                 log10gmin=5, log10gmax=10, ngamd=100)
 
-        model = ic.flux(outspec).to('1/(s cm2 keV)')
-
-        del ic # avoid memory leaks
+        model = ic.flux(outspec, distance=1*u.kpc).to('1/(s cm2 keV)')
 
         # Do a trapz integration to obtain the photons per bin
-        # TODO: implement in utils as in trapz_loglog
         if xhi is None:
-            photons = (outspec * model).to('1/(s cm2)').value
+            photons = (model * outspec).to('1/(s cm2)').value
         else:
-            photons = ((outspec[1:]-outspec[:-1]) *
-                       ((model[1:]+model[:-1])/2.)).to('1/(s cm2)').value
+            photons = trapz_loglog(model,outspec,intervals=True).to('1/(s cm2)').value
 
         if verbose:
             print self.thawedpars, trapz_loglog(outspec*model,outspec).to('erg/(s cm2)')
 
         return photons
 
-#class Synchrotron(ArithmeticModel):
-    #def __init__(self,name='IC'):
-        #self.index   = Parameter(name, 'index', 2.0, min=-10, max=10)
-        #self.ref     = Parameter(name, 'ref', 20, min=0, frozen=True)
-        #self.ampl    = Parameter(name, 'ampl', 1, min=0)
-        #self.cutoff  = Parameter(name, 'cutoff', 1e15, min=0,frozen=True)
-        #self.beta    = Parameter(name, 'beta', 1, min=0, max=10, frozen=True)
-        #self.B       = Parameter(name, 'B', 1, min=0, max=10, frozen=True)
-        #self.verbose = Parameter(name, 'verbose', 0, min=0, frozen=True)
-        #ArithmeticModel.__init__(self,name,(self.index,self.ref,self.ampl,self.cutoff,self.beta,self.B,self.verbose))
-        #self._use_caching = True
-        #self.cache = 10
+class Synchrotron(ArithmeticModel):
+    def __init__(self,name='IC'):
+        self.index   = Parameter(name, 'index', 2.0, min=-10, max=10)
+        self.ref     = Parameter(name, 'ref', 20, min=0, frozen=True, units='TeV')
+        self.ampl    = Parameter(name, 'ampl', 1, min=0, max=1e100, units='1/eV')
+        self.cutoff  = Parameter(name, 'cutoff', 0.0, min=0,frozen=True, units='TeV')
+        self.beta    = Parameter(name, 'beta', 1, min=0, max=10, frozen=True)
+        self.B       = Parameter(name, 'B', 1, min=0, max=10, frozen=True, units='G')
+        self.verbose = Parameter(name, 'verbose', 0, min=0, frozen=True)
+        ArithmeticModel.__init__(self,name,(self.index,self.ref,self.ampl,self.cutoff,self.beta,self.B,self.verbose))
+        self._use_caching = True
+        self.cache = 10
 
-    #def guess(self,dep,*args,**kwargs):
-        ## guess normalization from total flux
-        #xlo,xhi=args
-        #model=self.calc([p.val for p in self.pars],xlo,xhi)
-        #modflux=trapz_loglog(model,xlo)
-        #obsflux=trapz_loglog(dep*(xhi-xlo),xlo)
-        #self.ampl.set(self.ampl.val*obsflux/modflux)
+    def guess(self,dep,*args,**kwargs):
+        # guess normalization from total flux
+        xlo,xhi=args
+        model=self.calc([p.val for p in self.pars],xlo,xhi)
+        modflux=trapz_loglog(model,xlo)
+        obsflux=trapz_loglog(dep*(xhi-xlo),xlo)
+        self.ampl.set(self.ampl.val*obsflux/modflux)
 
-    #@modelCacher1d
-    #def calc(self,p,xlo,xhi):
+    @modelCacher1d
+    def calc(self,p,x,xhi=None):
 
-        #index,ref,ampl,cutoff,beta,B,verbose = p
+        index,ref,ampl,cutoff,beta,B,verbose = p
 
-        ## Sherpa provides xlo, xhi in KeV, we convert to eV and merge into a
-        ## single array
-        #outspec=_mergex(xlo,xhi)*1e3
+        # Sherpa provides xlo, xhi in KeV, we merge into a single array if bins required
+        if xhi is None:
+            outspec = x * u.keV
+        else:
+            outspec = _mergex(xlo,xhi) * u.keV
 
-        #ozm=ElectronOZM(outspec,
-                #ampl,
-                #index=index,
-                #norm_energy=ref*1e12,
-                #cutoff=cutoff*1e12,
-                #beta=beta,
-                #B=B,
-                #seedspec=['CMB',],
-                #nolog=True,
-                #gmin=1e5,
-                #gmax=1e10,
-                #ngamd=30,
-                #)
+        if cutoff == 0.0:
+            pdist = models.PowerLaw(ampl * u.Unit('1/eV'), ref * u.TeV, index)
+        else:
+            pdist = models.ExponentialCutoffPowerLaw(ampl * u.Unit('1/eV'),
+                    ref * u.TeV, index, cutoff * u.TeV, beta=beta)
 
-        #ozm.calc_sy()
-        #model=ozm.specsy
-        #del ozm # avoid memory leaks
+        sy = models.Synchrotron(pdist, B=B*u.G,
+                log10gmin=5, log10gmax=10, ngamd=50)
 
-        ## Do a trapz integration to obtain the photons per bin
-        #photons=(outspec[1:]-outspec[:-1])*((model[1:]+model[:-1])/2.)
+        model = sy.flux(outspec, distance=1*u.kpc).to('1/(s cm2 keV)')
 
-        #if verbose:
-            #print self.thawedpars, trapz_loglog((outspec*1.60217656e-12)*model,outspec)
+        # Do a trapz integration to obtain the photons per bin
+        if xhi is None:
+            photons = (model * outspec).to('1/(s cm2)').value
+        else:
+            photons = trapz_loglog(model,outspec,intervals=True).to('1/(s cm2)').value
 
-        #return photons
+        if verbose:
+            print self.thawedpars, trapz_loglog(outspec*model,outspec).to('erg/(s cm2)')
 
-#class PionDecay(ArithmeticModel):
-    #def __init__(self,name='pp'):
-        #self.index   = Parameter(name,  'index',   2.1,  min=-10,  max=10)
-        #self.ref     = Parameter(name,  'ref',     60,   min=0,    frozen=True)
-        #self.ampl    = Parameter(name,  'ampl',    100,    min=0)
-        #self.cutoff  = Parameter(name,  'cutoff',  0,    min=0,    frozen=True)
-        #self.beta    = Parameter(name,  'beta',    1,    min=0,    max=10,       frozen=True)
-        #self.verbose = Parameter(name, 'verbose', 0, min=0, frozen=True)
-        #ArithmeticModel.__init__(self,name,(self.index,self.ref,self.ampl,self.cutoff,self.beta,self.verbose))
-        #self._use_caching = True
-        #self.cache = 10
+        return photons
 
-    #def guess(self,dep,*args,**kwargs):
-        ## guess normalization from total flux
-        #xlo,xhi=args
-        #model=self.calc([p.val for p in self.pars],xlo,xhi)
-        #modflux=trapz_loglog(model,xlo)
-        #obsflux=trapz_loglog(dep*(xhi-xlo),xlo) 
-        #self.ampl.set(self.ampl.val*obsflux/modflux)
+class PionDecay(ArithmeticModel):
+    def __init__(self,name='pp'):
+        self.index   = Parameter(name , 'index'   , 2.1 , min=-10 , max=10)
+        self.ref     = Parameter(name , 'ref'     , 60  , min=0   , frozen=True    , units='TeV')
+        self.ampl    = Parameter(name , 'ampl'    , 100 , min=0   , units='1/eV')
+        self.cutoff  = Parameter(name , 'cutoff'  , 0   , min=0   , frozen=True    , units='TeV')
+        self.beta    = Parameter(name , 'beta'    , 1   , min=0   , max=10         , frozen=True)
+        self.nh      = Parameter(name , 'nH'      , 1   , min=0   , frozen=True    , units='1/cm3')
+        self.verbose = Parameter(name , 'verbose' , 0   , min=0   , frozen=True)
+        ArithmeticModel.__init__(self,name,(self.index,self.ref,self.ampl,self.cutoff,self.beta,self.nh,self.verbose))
+        self._use_caching = True
+        self.cache = 10
 
-    #@modelCacher1d
-    #def calc(self,p,xlo,xhi):
+    def guess(self,dep,*args,**kwargs):
+        # guess normalization from total flux
+        xlo,xhi=args
+        model=self.calc([p.val for p in self.pars],xlo,xhi)
+        modflux=trapz_loglog(model,xlo)
+        obsflux=trapz_loglog(dep*(xhi-xlo),xlo)
+        self.ampl.set(self.ampl.val*obsflux/modflux)
 
-        #index,ref,ampl,cutoff,beta,verbose = p
+    @modelCacher1d
+    def calc(self,p,x,xhi=None):
 
-        #if cutoff == 0:
-            #cutoff=None
-        #else:
-            #cutoff*=1e12
+        index,ref,ampl,cutoff,beta,nh,verbose = p
 
-        ## Sherpa provides xlo, xhi in KeV, we convert to eV and merge into a
-        ## single array
-        #outspec=_mergex(xlo,xhi)*1e3
+        # Sherpa provides xlo, xhi in KeV, we merge into a single array if bins required
+        if xhi is None:
+            outspec = x * u.keV
+        else:
+            outspec = _mergex(xlo,xhi) * u.keV
 
-        #ozm=ProtonOZM(outspec,
-                #ampl,
-                #index=index,
-                #norm_energy=ref*1e12,
-                #cutoff=cutoff,
-                #beta=beta,
-                #seedspec=['CMB',],
-                #nolog=True,
-                #Etrans=1e10,
-                #)
+        if cutoff == 0.0:
+            pdist = models.PowerLaw(ampl * u.Unit('1/eV'), ref * u.TeV, index)
+        else:
+            pdist = models.ExponentialCutoffPowerLaw(ampl * u.Unit('1/eV'),
+                    ref * u.TeV, index, cutoff * u.TeV, beta=beta)
 
-        #ozm.calc_outspec()
-        #model=ozm.specpp
-        #del ozm # avoid memory leaks
+        pp = models.PionDecay(pdist, nh=nh*u.Unit('1/cm3'))
 
-        ## Do a trapz integration to obtain the photons per bin
-        #photons=(outspec[1:]-outspec[:-1])*((model[1:]+model[:-1])/2.)
+        model = sy.flux(outspec, distance=1*u.kpc).to('1/(s cm2 keV)')
 
-        #if verbose:
-            #print self.thawedpars, trapz_loglog((outspec*1.60217656e-12)*model,outspec)
+        # Do a trapz integration to obtain the photons per bin
+        if xhi is None:
+            photons = (model * outspec).to('1/(s cm2)').value
+        else:
+            photons = trapz_loglog(model,outspec,intervals=True).to('1/(s cm2)').value
 
-        #return photons
+        if verbose:
+            print self.thawedpars, trapz_loglog(outspec*model,outspec).to('erg/(s cm2)')
+
+        return photons
