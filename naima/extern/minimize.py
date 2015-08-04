573594732f7a678e37f654f6fb47c564995384ab
Víctor Zabalza <vzabalza@gmail.com>
2015-08-04 18:14:25 +0100

    new prefit with relative tolerance termination

--- naima/extern/minimize.py
+++ naima/extern/minimize.py
@@ -36,54 +36,8 @@ def wrap_function(function, args):
 
 class OptimizeResult(dict):
     """ Represents the optimization result.
-
-    Attributes
-    ----------
-    x : ndarray
-        The solution of the optimization.
-    success : bool
-        Whether or not the optimizer exited successfully.
-    status : int
-        Termination status of the optimizer. Its value depends on the
-        underlying solver. Refer to `message` for details.
-    message : str
-        Description of the cause of the termination.
-    fun, jac, hess, hess_inv : ndarray
-        Values of objective function, Jacobian, Hessian or its inverse (if
-        available). The Hessians may be approximations, see the documentation
-        of the function in question.
-    nfev, njev, nhev : int
-        Number of evaluations of the objective functions and of its
-        Jacobian and Hessian.
-    nit : int
-        Number of iterations performed by the optimizer.
-    maxcv : float
-        The maximum constraint violation.
-
-    Notes
-    -----
-    There may be additional attributes not listed above depending of the
-    specific solver. Since this class is essentially a subclass of dict
-    with attribute accessors, one can see which attributes are available
-    using the `keys()` method.
     """
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
-
-    __setattr__ = dict.__setitem__
-    __delattr__ = dict.__delitem__
-
-    def __repr__(self):
-        if self.keys():
-            m = max(map(len, list(self.keys()))) + 1
-            return '\n'.join([k.rjust(m) + ': ' + repr(v)
-                              for k, v in self.items()])
-        else:
-            return self.__class__.__name__ + "()"
-
+    pass
 
 class OptimizeWarning(UserWarning):
     pass
@@ -93,7 +47,7 @@ def minimize(func,x0,args=(),options={},method=None):
     return _minimize_neldermead(func, x0, args=args, **options)
 
 def _minimize_neldermead(func, x0, args=(), callback=None, xtol=1e-4, ftol=1e-4,
-        maxiter=None, maxfev=None, disp=False, return_all=False):
+        maxiter=None, maxfev=None, disp=False, return_all=False): # pragma: no cover
     """
     Minimization of scalar function of one or more variables using the
     Nelder-Mead algorithm.
