5d87f52f9b293b874c5bea4f2f75e8c58b9c24c6
Víctor Zabalza <vzabalza@gmail.com>
2015-08-12 14:29:09 +0100

    add reasonable parameter ranges and clean namespace

--- naima/model_widget.py
+++ naima/model_widget.py
@@ -19,18 +19,17 @@ class ModelWidget(object):
             labels=None, sed=True, auto_update=True, e_npoints=100):
 
         npars = len(p0)
-        self.labels = labels
-        if self.labels is None:
-            self.labels = ['par{0}'.format(i) for i in range(npars)]
+        if labels is None:
+            labels = ['par{0}'.format(i) for i in range(npars)]
         elif len(labels) < npars:
-            self.labels += ['par{0}'.format(i) for i in range(len(labels),npars)]
+            labels += ['par{0}'.format(i) for i in range(len(labels),npars)]
 
         hasdata = data is not None
 
         self.modelfn = modelfn
 
         self.fig = plt.figure()
-        self.modelax = plt.subplot2grid((2*npars+1,4),(0,0),rowspan=npars,colspan=4)
+        modelax = plt.subplot2grid((2*npars+1,4),(0,0),rowspan=npars,colspan=4)
 
         if e_range:
             e_range = validate_array('e_range', u.Quantity(e_range),
@@ -48,7 +47,7 @@ class ModelWidget(object):
 
         if hasdata:
             data = validate_data_table(data)
-            _plot_data_to_ax(data, self.modelax, sed=sed)
+            _plot_data_to_ax(data, modelax, sed=sed)
             if not e_range:
                 energy = data['energy']
                 flux = data['flux']
@@ -59,48 +58,61 @@ class ModelWidget(object):
 
         self.f_unit, self.sedf = sed_conversion(energy, model.unit, sed)
         if hasdata:
-            self.modelax.set_xlim(
+            modelax.set_xlim(
                     (data['energy'][0] - data['energy_error_lo'][0]).value / 3,
                     (data['energy'][-1] + data['energy_error_hi'][-1]).value * 3)
         else:
             # plot_data_to_ax has not set ylabel
             unit = self.f_unit.to_string('latex_inline')
             if sed:
-                self.modelax.set_ylabel(r'$E^2 dN/dE [{0}]$'.format(unit))
+                modelax.set_ylabel(r'$E^2 dN/dE [{0}]$'.format(unit))
             else:
-                self.modelax.set_ylabel(r'$dN/dE [{0}]$'.format(unit))
-            self.modelax.set_xlim(data['energy'][0].value/3,
+                modelax.set_ylabel(r'$dN/dE [{0}]$'.format(unit))
+            modelax.set_xlim(data['energy'][0].value/3,
                     data['energy'][-1].value*3)
 
-        self.line, = self.modelax.loglog(energy,
+        self.line, = modelax.loglog(energy,
                 (model*self.sedf).to(self.f_unit), lw=2,
                 c='k', zorder=10)
 
-        self.modelax.set_xlabel(
+        modelax.set_xlabel(
                 'Energy [{0}]'.format(energy.unit.to_string('latex_inline')))
 
 
-        self.paraxes = []
+        paraxes = []
         for n in range(npars):
-            self.paraxes.append(plt.subplot2grid((2*npars+1,4),(1+npars+n,0),colspan=2))
+            paraxes.append(plt.subplot2grid((2*npars+1,4),(1+npars+n,0),colspan=2))
         self.parsliders = []
-        for label,parax,valinit in six.moves.zip(self.labels, self.paraxes, p0):
-            slider = Slider(parax, label, valinit/10, valinit*3,
+        for label,parax,valinit in six.moves.zip(labels, paraxes, p0):
+            # Attempt to estimate reasonable parameter ranges from label
+            pmin, pmax = valinit/10, valinit*3
+            if 'log' in label:
+                pmin = valinit - 2
+                pmax = valinit + 2
+            elif ('index' in label) or ('alpha' in label):
+                if valinit > 0.:
+                    pmin = -2
+                    pmax = 5
+                else:
+                    pmin = -5
+                    pmax = 2
+
+            slider = Slider(parax, label, pmin, pmax,
                 valinit=valinit, valfmt='%g',closedmin=False, closedmax=False)
             slider.on_changed(self.update_if_auto)
             self.parsliders.append(slider)
 
-        self.updateax = plt.subplot2grid((2*npars+1,4),(npars+1,3),colspan=1,
+        updateax = plt.subplot2grid((2*npars+1,4),(npars+1,3),colspan=1,
                 rowspan=min(int(npars/2),2))
-        self.update_button = Button(self.updateax, 'Update model')
-        self.update_button.on_clicked(self.update)
+        update_button = Button(updateax, 'Update model')
+        update_button.on_clicked(self.update)
 
-        self.autoupdateax = plt.subplot2grid((2*npars+1,4),
+        autoupdateax = plt.subplot2grid((2*npars+1,4),
                 (npars+1+min(int(npars/2),2),3),colspan=1,
                 rowspan=min(int(npars/2),2))
-        self.auto_update_check = CheckButtons(self.autoupdateax,
+        auto_update_check = CheckButtons(autoupdateax,
                 ('Auto update',), (auto_update,))
-        self.auto_update_check.on_clicked(self.update_autoupdate)
+        auto_update_check.on_clicked(self.update_autoupdate)
         self.autoupdate = auto_update
 
         self.fig.subplots_adjust(top=0.98,right=0.98,bottom=0.02,hspace=0.2)
@@ -115,8 +127,8 @@ class ModelWidget(object):
             self.update(val)
 
     def update(self,event):
-        pars = [slider.val for slider in self.parsliders]
-        model = self.modelfn(pars, self.data_for_model)[0]
+        self.pars = [slider.val for slider in self.parsliders]
+        model = self.modelfn(self.pars, self.data_for_model)[0]
         self.line.set_ydata((model*self.sedf).to(self.f_unit))
         self.fig.canvas.draw_idle()
 
