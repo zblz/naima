36203cb07f4185bc6fa7b7acac831b09a8f0b46b
tialis <goldstein2034@hotmail.com>
2016-06-01 15:46:56 +0200

    - Cleaned a bit examples/absorbed_SynIC.py

--- examples/absorbed_SynIC.py
+++ examples/absorbed_SynIC.py
@@ -9,7 +9,7 @@ import naima
 
 from naima.models import InverseCompton, Synchrotron, ExponentialCutoffPowerLaw, BrokenPowerLaw, AbsorptionModel
 
-#    
+# Define the redshift of the source, and absorption model
 redshift = 0.06
 EBL_transmitance = AbsorptionModel(redshift, 'Dominguez')
 
@@ -21,16 +21,11 @@ def ElectronSynIC(pars, data):
     alpha1 = pars[2]
     alpha2 = pars[3]
     B = pars[4] * u.uG
-#     redshift = pars[5]
 
     # Initialize instances of the particle distribution and radiative models
     BPL = BrokenPowerLaw(amplitude, 1. * u.TeV, e_break, alpha1, alpha2)
     
-    # Load absorption model for the specified redshift
-#      EBL_transmitance = AbsorptionModel(redshift, 'Dominguez')
-    
-    # Compute IC on CMB and on a FIR component with values from GALPROP for the
-    # position of RXJ1713
+    # Compute IC on CMB and on a FIR component
     IC = InverseCompton(
         BPL,
         seed_photon_fields=['CMB'],
@@ -38,8 +33,6 @@ def ElectronSynIC(pars, data):
     SYN = Synchrotron(BPL, B=B)
 
     # compute flux at the energies given in data['energy']
-    model = (IC.flux(data, distance=1.0 * u.kpc) + SYN.flux(data,
-                                                      distance=1.0 * u.kpc))
     model = (EBL_transmitance.transmission(data) * IC.flux(data, distance=1.0 * u.kpc) + SYN.flux(data,
                                                       distance=1.0 * u.kpc))
 
@@ -52,10 +45,12 @@ def ElectronSynIC(pars, data):
 
 if __name__ == '__main__':
 
+    # Some random values for a "beautiful double peak structure
     p0 = np.array((33., 1., 0.35, 3., 5))
 
     labels = ['log10(norm)', 'log10(Energy_Break)', 'index1', 'index2', 'B', 'redshift']
 
+    # Run interactive fitter, to show the very high energy absorption 
     imf = naima.InteractiveModelFitter(ElectronSynIC, p0, labels=labels, e_range=[1e-09*u.GeV, 1e05*u.GeV])
 
 
