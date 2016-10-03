fc25ea4c1b6ea74d123b7248507a59c85323579d
tialis <goldstein2034@hotmail.com>
2016-10-03 12:41:09 +0200

    Improved code style

--- examples/absorbed_SynIC.py
+++ examples/absorbed_SynIC.py
@@ -5,9 +5,10 @@ from astropy.io import ascii
 
 import naima
 
-## Model definition
+# Model definition
 
 from naima.models import InverseCompton, Synchrotron, ExponentialCutoffPowerLaw, BrokenPowerLaw, EblAbsorptionModel
+
 
 def ElectronEblAbsorbedSynIC(pars, data):
 
@@ -17,14 +18,14 @@ def ElectronEblAbsorbedSynIC(pars, data):
     alpha1 = pars[2]
     alpha2 = pars[3]
     B = pars[4] * u.uG
-    
+
     # Define the redshift of the source, and absorption model
     redshift = pars[5] * u.dimensionless_unscaled
     EBL_transmitance = EblAbsorptionModel(redshift, 'Dominguez')
 
     # Initialize instances of the particle distribution and radiative models
     BPL = BrokenPowerLaw(amplitude, 1. * u.TeV, e_break, alpha1, alpha2)
-    
+
     # Compute IC on a CMB component
     IC = InverseCompton(
         BPL,
@@ -33,14 +34,13 @@ def ElectronEblAbsorbedSynIC(pars, data):
     SYN = Synchrotron(BPL, B=B)
 
     # compute flux at the energies given in data['energy']
-    model = (EBL_transmitance.transmission(data) * IC.flux(data, distance=1.0 * u.kpc) + 
+    model = (EBL_transmitance.transmission(data) * IC.flux(data, distance=1.0 * u.kpc) +
              SYN.flux(data, distance=1.0 * u.kpc))
 
     # The first array returned will be compared to the observed spectrum for
     # fitting. All subsequent objects will be stored in the sampler metadata
     # blobs.
     return model, IC.compute_We(Eemin=1 * u.TeV)
-
 
 
 if __name__ == '__main__':
@@ -50,7 +50,5 @@ if __name__ == '__main__':
 
     labels = ['log10(norm)', 'log10(Energy_Break)', 'index1', 'index2', 'B', 'redshift']
 
-    # Run interactive fitter, to show the very high energy absorption 
+    # Run interactive fitter, to show the very high energy absorption
     imf = naima.InteractiveModelFitter(ElectronEblAbsorbedSynIC, p0, labels=labels, e_range=[1e-09*u.GeV, 1e05*u.GeV])
-
-
